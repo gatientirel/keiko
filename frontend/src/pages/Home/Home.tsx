2b6deed3857dc86877c8153384a7beb5e71a0035
Gatien TIREL <gatien.tirel@theodo.com>
2026-05-06 16:42:43 +0200

    feature(Home): remove input

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState, ChangeEvent, useEffect } from "react"
+import { useState, useEffect } from "react"
 import { Pokemon } from "../../components/Pokemon"
 import styles from "./Home.module.css"
 
@@ -14,18 +14,6 @@ const endpoints = {
   allPokemons: "/pokemons",
 }
 
-const normalizeStr = (str: string) =>
-  str
-    .toLowerCase()
-    .normalize("NFD") // separate diachritics
-    .replace(/[\u0300-\u036f]/g, "") // keep only letters and numbers
-    .replace(/[^a-z0-9]+/g, "-") // replace spaces by -
-    .replace(/^-+|-+$/g, "") // remove trailing / starting -
-
-function filterPokemonsByName(pokemons: PokemonInfo[], name: string) {
-  return pokemons.filter(pokemon => normalizeStr(pokemon.name).includes(normalizeStr(name)))
-}
-
 const fetchPokemons = async () => {
   const response = await fetch(`${BASE_URL_POKEMON_API}${endpoints.allPokemons}`, {
     headers: { accept: "application/json" },
@@ -34,10 +22,7 @@ const fetchPokemons = async () => {
 }
 
 export const Home = () => {
-  const [filterValue, setFilterValue] = useState("")
   const [pokemonList, setPokemonList] = useState<PokemonInfo[]>([])
-
-  const onInputChange = useCallback((e: ChangeEvent<HTMLInputElement>) => setFilterValue(e.target.value), [])
 
   useEffect(() => {
     fetchPokemons().then(pokemonData => setPokemonList(pokemonData))
@@ -46,12 +31,8 @@ export const Home = () => {
   return (
     <div className={styles.intro}>
       <div>Pokédex !</div>
-      <label htmlFor={"input-pokemon-name"} className={styles.label}>
-        Pokemon Name
-      </label>
-      <input id={"input-pokemon-name"} className={styles.input} onChange={onInputChange} value={filterValue} />
       <div>
-        {filterPokemonsByName(pokemonList, filterValue).map(pokemon => (
+        {pokemonList.map(pokemon => (
           <Pokemon key={pokemon.id} pokemon={pokemon} />
         ))}
       </div>

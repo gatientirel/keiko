6a3d9726cffd20d5f417fb0a5989885c6347098f
Gatien TIREL <gatien.tirel@theodo.com>
2026-05-06 14:44:18 +0200

    feature(Home): use pokemonList

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,15 +1,21 @@
 import { Pokemon } from "../../components/Pokemon"
 import styles from "./Home.module.css"
 
+const pokemonList = [
+  { name: "Carapuce", id: 7 },
+  { name: "Carabaffe", id: 8 },
+  { name: "Tortank", id: 9 },
+]
+
 export const Home = () => {
   return (
     <div className={styles.intro}>
       <div>Bienvenue sur ton futur pokédex !</div>
       <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React, Redux et Symfony, et attraper des pokemons !</div>
       <div>
-        <Pokemon name={"Carapuce"} id={7} />
-        <Pokemon name={"Carabaffe"} id={8} />
-        <Pokemon name={"Tortank"} id={9} />
+        {pokemonList.map(pokemon => (
+          <Pokemon key={pokemon.id} name={pokemon.name} id={pokemon.id} />
+        ))}
       </div>
     </div>
   )

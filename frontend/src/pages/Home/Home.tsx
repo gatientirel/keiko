bad45fbbd0a0e08ac55470611cf836771780a900
Gatien TIREL <gatien.tirel@theodo.com>
2026-05-06 18:24:19 +0200

    feature(Home): Error state handling

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -25,18 +25,28 @@ const fetchPokemons = async () => {
 export const Home = () => {
   const [pokemonList, setPokemonList] = useState<PokemonInfo[]>([])
   const [isLoading, setIsLoading] = useState(true)
+  const [isInError, setIsInError] = useState(false)
 
   useEffect(() => {
-    fetchPokemons().then(pokemonData => {
-      setPokemonList(pokemonData)
-      setIsLoading(false)
-    })
+    fetchPokemons()
+      .then(pokemonData => {
+        setPokemonList(pokemonData)
+      })
+      .catch(() => {
+        setIsInError(true)
+      })
+      .finally(() => setIsLoading(false))
   }, [])
 
   return (
     <div className={styles.intro}>
-      <div>Pokédex !</div>
-      <div className={styles.loader_container}>{isLoading && <Loader />}</div>
+      <h2>Pokédex !</h2>
+      <div className={styles.query_state_info_container}>
+        {isLoading && <Loader />}
+        {isInError && (
+          <p className={styles.error_message}>Oops ! All pokemons went missing because of some unknown event...</p>
+        )}
+      </div>
       <div className={styles.pokemon_cards_container}>
         {pokemonList.map(pokemon => (
           <Pokemon key={pokemon.id} pokemon={pokemon} />

c7eaea15abce1bfda5cd1800978adcfb4e65bf8c
Gatien TIREL <gatien.tirel@theodo.com>
2026-05-06 15:28:39 +0200

    feature(PokemonList): pokemons fetched from api + input standard

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState, ChangeEvent } from "react"
+import { useCallback, useState, ChangeEvent, useEffect } from "react"
 import { Pokemon } from "../../components/Pokemon"
 import styles from "./Home.module.css"
 
@@ -7,11 +7,17 @@ export interface Pokemon {
   id: number
 }
 
-const pokemonList: Pokemon[] = [
-  { name: "Carapuce", id: 7 },
-  { name: "Carabaffe", id: 8 },
-  { name: "Tortank", id: 9 },
-]
+export interface PokemonInfo {
+  name: string
+  id: number
+  height: number
+  weight: number
+}
+
+const BASE_URL_POKEMON_API = "http://localhost:8000"
+const endpoints = {
+  allPokemons: "/pokemons",
+}
 
 const normalizeStr = (str: string) =>
   str
@@ -27,13 +33,22 @@ function filterPokemonsByName(pokemons: Pokemon[], name: string) {
 
 export const Home = () => {
   const [filterValue, setFilterValue] = useState("")
+  const [pokemonList, setPokemonList] = useState<PokemonInfo[]>([])
 
   const onInputChange = useCallback((e: ChangeEvent<HTMLInputElement>) => setFilterValue(e.target.value), [])
+
+  useEffect(() => {
+    fetch(`${BASE_URL_POKEMON_API}${endpoints.allPokemons}`, { headers: { accept: "application/json" } })
+      .then(response => response.json())
+      .then(pokemonData => setPokemonList(pokemonData))
+  }, [])
+
   return (
     <div className={styles.intro}>
       <div>Bienvenue sur ton futur pokédex !</div>
       <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React, Redux et Symfony, et attraper des pokemons !</div>
-      <input className={styles.input} onChange={onInputChange} value={filterValue} />
+      <label htmlFor={"input-pokemon-name"}>Pokemon Name</label>
+      <input id={"input-pokemon-name"} className={styles.input} onChange={onInputChange} value={filterValue} />
       <div>
         {filterPokemonsByName(pokemonList, filterValue).map(pokemon => (
           <Pokemon key={pokemon.id} pokemon={pokemon} />
